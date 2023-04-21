f82d26f8367d7e99e736c9498aedeaab350a9ba5
Dhvani Vora <dvora@splunk.com>
2023-04-21 18:36:31 +0530

    Added copyright to files

--- tests/test_get_iocs.py
+++ tests/test_get_iocs.py
@@ -1,3 +1,8 @@
+# File: test_get_iocs.py
+#
+# Copyright (c) VMRay GmbH 2017-2023
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 import pytest
 
 from tests.data_for_testing import SAMPLE_IOCS_REST_API_RESULT, SAMPLE_IOCS_RESULT
--- tests/test_rest_api_get_iocs.py
+++ tests/test_rest_api_get_iocs.py
@@ -1,3 +1,8 @@
+# File: test_rest_api_get_iocs.py
+#
+# Copyright (c) VMRay GmbH 2017-2023
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 import pytest
 
 from rest_cmds import VMRay
